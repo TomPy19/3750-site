8d4f4db5a08d1890d8192afbcba7a522d2356262
TomPy19 <tgperso@clemson.edu>
2023-11-28 09:22:03 -0500

    yay merging

--- pages/015836/loops.js
+++
@@ -1,8 +0,0 @@
-import {genCardArr} from "./cards.js";
-
-var showBtn = document.getElementById("show-all");
-showBtn.onclick = () => {
-  showBtn.disabled = true;
-  var names = genCardArr();
-  for (var i=0; i < names.length; i++) names[i].printCard();
-}
